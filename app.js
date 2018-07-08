66505a293e3dfcd502277121c1781faf0157a31a
jesseflores512 <jesseflores512@gmail.com>
2018-07-08 15:54:13 -0500

    api connected

--- app.js
+++ app.js
@@ -6,12 +6,6 @@ $(function(){
 
 		var hero = $("#heroInput").val().toLowerCase();
 
-		var apiKey = "0d15eb0602df5de0c44219c2eb35237b"
-
-		var apiPrKey = "9806f5868f2685972e72cc8aa2732e7ade19fd48"
-
-		var hash = "51f99abd2a852eb72f4b02dfe526e620"
-
 		var queryURL = "https://gateway.marvel.com/v1/public/characters?name=" + hero + "&ts=1&apikey=" + apiKey + "&hash=" + hash;
 
 		function showInfo(data){
